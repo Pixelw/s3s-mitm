b0b976d3abe34e0d9af63a11412deaa044dab4c1
Pixelw <carl-suly@live.cn>
2025-02-07 12:00:06 +0800

    it works...!

--- nso-trigger.py
+++ nso-trigger.py
@@ -1,5 +1,6 @@
 import json
 import subprocess
+from datetime import datetime
 
 import mitmproxy.http
 
@@ -17,19 +18,30 @@ class Trigger:
             if gtoken is None or len(gtoken) == 0:
                 return
 
-            print("[i] gtoken found!")
+            print(f"[i] gtoken found!, {gtoken[:10]}...")
             json_data = json.loads(flow.response.get_text())
             if "bulletToken" not in json_data:
                 return
 
             bullet_token = json_data["bulletToken"]
-            print("[i] bullet token found!")
-            with open("config.txt", "r+", encoding="utf-8") as f:
-                config = json.loads(f)
-                config['gtoken'] = gtoken
-                config['bullettoken'] = bullet_token
-                f.write(json.dumps(config))
-                # TODO trigger s3s
-                subprocess.run(["python", "s3s.py", "-r"])
+            print(f"[i] bullet token found!, {bullet_token[:10]}...")
+            with open("config.txt", "r", encoding="utf-8") as f:
+                config = json.load(f)
+
+            config['gtoken'] = gtoken
+            config['bullettoken'] = bullet_token
+            now = datetime.now()
+            config['nso_last_trigger'] = now.strftime("%Y-%m-%d %H:%M:%S %Z")
+
+            with open("config.txt", "w", encoding="utf-8") as w:
+                json.dump(config, w, indent=4, ensure_ascii=False)
+
+            # trigger s3s -r
+            subprocess.run(["python", "s3s.py", "-r"])
         except Exception as e:
             print(f"[E] parse bulletToken failed: {e}")
+
+
+addons = [
+    Trigger()
+]
